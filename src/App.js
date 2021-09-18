e43bbda477d6028416c978187d0e4985d14d337c
SAGOR <seakingmawmaw2020@gmail.com>
2021-09-19 00:49:44 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { PhotoCamera } from "@mui/icons-material";
+import {
+  AppBar,
+  CssBaseline,
+  Typography,
+  Toolbar,
+  Container,
+  Grid,
+  Button,
+} from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo world</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm" style={{ marginTop: "100px" }}>
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              My photo
+            </Typography>
+            <Typography
+              variant="h6"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello people i am just tryng how can i use material ui in my
+              project thats why i need to practis more and this web site is one
+              of my practise projcet
+            </Typography>
+            <div>
+              <Grid container spacing={2} justify="center">
+                <Grid item xs={6} align="end">
+                  <Button variant="contained" color="primary">
+                    See my photo
+                  </Button>
+                </Grid>
+                <Grid item xs={6} align="start">
+                  <Button variant="outlined" color="primary">
+                    Secondary Action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </>
   );
 }
 
