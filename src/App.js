0329588fe39a48a4214c3f89644a1bf32bea877e
SAGOR <seakingmawmaw2020@gmail.com>
2021-09-19 22:47:00 +0600

    update card

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ import {
   Container,
   Grid,
   Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
 } from "@mui/material";
 import "./App.css";
+import useStyles from "./styles";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function App() {
+  const styles = useStyles();
   return (
     <>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera className={styles.icon} />
           <Typography variant="h6">Photo world</Typography>
         </Toolbar>
       </AppBar>
       <main>
-        <div>
-          <Container maxWidth="sm" style={{ marginTop: "100px" }}>
+        <div className={styles.topPart}>
+          <Container maxWidth="sm">
             <Typography
               variant="h2"
               align="center"
@@ -36,12 +44,13 @@ function App() {
               align="center"
               color="textSecondary"
               paragraph
+              className="styles.bgStyle"
             >
               Hello people i am just tryng how can i use material ui in my
               project thats why i need to practis more and this web site is one
               of my practise projcet
             </Typography>
-            <div>
+            <div className={styles.button}>
               <Grid container spacing={2} justify="center">
                 <Grid item xs={6} align="end">
                   <Button variant="contained" color="primary">
@@ -57,6 +66,35 @@ function App() {
             </div>
           </Container>
         </div>
+        <Container className={styles.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} md={4} sm={6}>
+                <Card className={styles.card}>
+                  <CardMedia
+                    className={styles.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="image title"
+                  />
+                  <CardContent className={styles.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                      this is the card
+                    </Typography>
+                    <Typography>this is media card i am using</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View{" "}
+                    </Button>
+                    <Button size="small" color="primary">
+                      more{" "}
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
     </>
   );
---
+++ src/styles.js
@@ -0,0 +1,29 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  topPart: {
+    marginTop: "100px",
+  },
+  icon:{
+      marginRight: "20px",
+  },
+  button: {
+      marginTop: "40px",
+  },
+  cardGrid:{
+    padding:"20px 0"
+  },
+  card:{
+    height:"100%",
+    display:"flex",
+    flexDirection:"column",
+  },
+  cardMedia:{
+    paddingTop:"56.24%"
+  },
+  cardContent:{
+    flexGrow:1,
+  }
+});
+
+export default useStyles;
